5bb5fe68ae8f22f9bb88e75bb92aa03b288a7baf
NRahul1125 <luckynakidi11@gmail.com>
2025-04-23 21:50:06 +0530

    .

--- WeatherApp/venv/Scripts/activate.bat
+++ WeatherApp/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Rahul\OneDrive\Desktop\WeatherApp\venv
+set VIRTUAL_ENV=C:\Users\Rahul\OneDrive\Desktop\BDA\BDA-DS\Weather-Pattern-Prediction-Using-Big-Data-Analytics-and-Hadoop\WeatherApp\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
